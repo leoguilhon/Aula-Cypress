267f3bb8ca9e7a2e39cd05297b7856d213446933
Leonardo Guilhon <leoguilhon@gmail.com>
2023-03-21 22:46:45 -0300

    Comandos para o endereço

--- teste-ebac-ui/cypress/e2e/endereco.cy.js
+++ teste-ebac-ui/cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from '../support/page-objects/endereco.page';
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -6,7 +8,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         cy.login('aluno_ebac@teste.com', 'teste@teste.com')
     });
     
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        //cadastro de endereço
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Flávio', 'Silva', 'Americanas', 'Brasil', 'Av Brasil', '3100', 'São Paulo', 'São Paulo', '22255-080', '21975634569', 'flavio@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
