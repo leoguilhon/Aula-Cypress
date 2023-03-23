c2ffbf08f1b60d4edb69143e383b6f922862e0d0
Leonardo Guilhon <leoguilhon@gmail.com>
2023-03-23 13:52:24 -0300

    Adicionado comando de endereço de entrega

--- teste-ebac-ui/cypress/e2e/endereco.cy.js
+++ teste-ebac-ui/cypress/e2e/endereco.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 import enderecoPage from '../support/page-objects/endereco.page';
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -8,8 +9,43 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         cy.login('aluno_ebac@teste.com', 'teste@teste.com')
     });
     
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Flávio', 'Silva', 'Americanas', 'Brasil', 'Av Brasil', '3100', 'São Paulo', 'São Paulo', '22255-080', '21975634569', 'flavio@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+    it('Deve fazer cadastro de entrega com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+    it('Deve fazer cadastro de entrega com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega('Flávio', 'Silva', 'Americanas', 'Brasil', 'Av Brasil', '3100', 'São Paulo', 'São Paulo', '22255-080')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
